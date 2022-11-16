e31c17a41b847cf96b7f837791e7b1fe502c6607
Janos Nanai <janos.nanai@gmail.com>
2022-11-16 16:37:37 +0100

    form-modal validation error and styling update

--- src/components/modals/form.modal.tsx
+++ src/components/modals/form.modal.tsx
@@ -57,7 +57,7 @@ function FormModal({
     <Dialog open={isOpen} onClose={onClose} as="div" className="relative z-10">
       <div className="fixed inset-0 flex items-center justify-center bg-black/50 p-4 backdrop-blur-sm">
         <Dialog.Panel className="w-full max-w-md overflow-y-auto rounded-lg bg-zinc-900 p-4 shadow-lg">
-          <div className="mb-5 flex justify-between">
+          <div className="mb-9 flex justify-between">
             <Dialog.Title className="text-2xl uppercase text-zinc-100">
               {title}
             </Dialog.Title>
@@ -69,38 +69,55 @@ function FormModal({
             </button>
           </div>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="space-y-3">
-              <div>
-                <div className="flex items-center">
-                  <span className="text-lg text-zinc-400">{BASE_URL}</span>
-                  <input
-                    type="text"
-                    placeholder="enter slug or generate one..."
-                    {...register("slug")}
-                    className="grow rounded-lg border border-zinc-800 bg-zinc-1000 px-2 py-1 text-zinc-100"
-                  />
+            <div className="space-y-9">
+              <div className="space-y-1">
+                <div>
+                  <div className="flex items-center">
+                    <span className="text-lg text-zinc-400">{BASE_URL}</span>
+                    <input
+                      autoComplete="off"
+                      type="text"
+                      placeholder="enter slug or generate one..."
+                      {...register("slug")}
+                      className={`grow rounded-lg border bg-zinc-1000 px-2 py-1 text-zinc-100 ${
+                        validationErrors.slug
+                          ? "border-red-500"
+                          : "border-zinc-800"
+                      }`}
+                    />
+                  </div>
+                  <p className="h-4 text-right text-xs font-semibold tracking-wide text-red-500">
+                    {validationErrors.slug?.message}
+                  </p>
                 </div>
-                <p>{validationErrors.slug?.message}</p>
                 <ButtonSecondary onClick={handleGenerate} className="w-full">
                   generate
                 </ButtonSecondary>
               </div>
               <div>
                 <input
+                  autoComplete="off"
                   type="text"
                   placeholder="enter your url..."
                   {...register("url")}
-                  className="w-full rounded-lg border border-zinc-800 bg-zinc-1000 px-2 py-1 text-zinc-100"
+                  className={`w-full rounded-lg border bg-zinc-1000 px-2 py-1 text-zinc-100 ${
+                    validationErrors.url ? "border-red-500" : "border-zinc-800"
+                  }`}
                 />
-                <p>{validationErrors.url?.message}</p>
+                <p className="-mb-4 h-4 text-right text-xs font-semibold tracking-wide text-red-500">
+                  {validationErrors.url?.message}
+                </p>
               </div>
               <div>
                 <textarea
                   rows={5}
+                  placeholder="enter description... (optional)"
                   {...register("description")}
                   className="max-h-64 min-h-[5rem] w-full rounded-lg border border-zinc-800 bg-zinc-1000 px-2 py-1 text-zinc-100"
                 />
-                <p>{validationErrors.description?.message}</p>
+                <p className="-mb-4 h-4 text-right text-xs font-semibold tracking-wide text-red-500">
+                  {validationErrors.description?.message}
+                </p>
               </div>
               <ButtonPrimary type="submit" className="w-full">
                 submit
