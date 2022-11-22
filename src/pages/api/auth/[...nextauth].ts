b26f517c5ee3392ab0ff8b1a342257438e6315ef
Janos Nanai <janos.nanai@gmail.com>
2022-11-22 17:56:50 +0100

    feat: discord login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,41 +1,17 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-// import DiscordProvider from "next-auth/providers/discord";
+import DiscordProvider from "next-auth/providers/discord";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
 
-// export const authOptions: NextAuthOptions = {
-//   // Include user.id on session
-//   callbacks: {
-//     session({ session, user }) {
-//       if (session.user) {
-//         session.user.id = user.id;
-//       }
-//       return session;
-//     },
-//   },
-//   // Configure one or more authentication providers
-//   adapter: PrismaAdapter(prisma),
-//   providers: [
-//     DiscordProvider({
-//       clientId: env.DISCORD_CLIENT_ID,
-//       clientSecret: env.DISCORD_CLIENT_SECRET,
-//       authorization: { params: { scope: "identify" } },
-//     }),
-//     // ...add more providers here
-//   ],
-//   secret: env.NEXTAUTH_SECRET,
-// };
-
 export const authOptions: NextAuthOptions = {
   providers: [
-    // DiscordProvider({
-    //   clientId: String(env.DISCORD_CLIENT_ID),
-    //   clientSecret: env.DISCORD_CLIENT_SECRET,
-    //   // authorization: { params: { scope: "identify" } },
-    // }),
+    DiscordProvider({
+      clientId: String(env.DISCORD_CLIENT_ID),
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
     GithubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
